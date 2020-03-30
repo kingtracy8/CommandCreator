028079854e3b97ab9eb3bf48feb75a780023aa72
kingtracy8 <kingtracy8@163.com>
2020-03-30 16:17:18 +0800

    报表开发

--- src/main/java/com/tracy/utils/ExcelCURD.java
+++ src/main/java/com/tracy/utils/ExcelCURD.java
@@ -6,7 +6,6 @@ import com.spire.xls.FileFormat;
 import com.spire.xls.Workbook;
 import com.spire.xls.Worksheet;
 
-
 /**
  * Created by trcay on 2020/3/29.
  * description:对excel表进行增删改查操作
@@ -23,6 +22,7 @@ public class ExcelCURD {
 
         //加载文档
         Workbook wb = new Workbook();
+        System.out.println("加载OLT流量报表....");
         wb.loadFromFile("F:\\Datareport\\OLT.xlsx");
 
         //获取工作表
@@ -34,14 +34,16 @@ public class ExcelCURD {
         //保存文档
         wb.saveToFile("F:\\Datareport\\OLT流量报表.xlsx");
         wb.dispose();
-        System.out.println("生成成功，目录：F:\\Datareport\\OLT流量报表.xlsx");
+        System.out.println("OLT流量报表格式整理完成....");
     }
 
     public static void deleteDswCol() {
 
         Workbook wb = new Workbook();
 
-        //假如后缀是.xls格式，结果只能返回200行，原因不明  linsong_wei@163.com
+        //假如后缀是.xls格式，结果只能返回200行，原因不明  linsong_wei@163.com  ， xlsx文件可处理大于200行数据
+
+        System.out.println("加载汇聚交换机流量报表....");
 
         wb.loadFromFile("F:\\Datareport\\DSW.xlsx");
 
@@ -61,7 +63,7 @@ public class ExcelCURD {
         //由于原表里 平均流出流速 和 平均日峰值流出利用率 是反过来的，所以需要将这两列互换
         RotateCol();
 
-        System.out.println("生成成功，目录：F:\\Datareport\\汇聚交换机流量报表.xlsx");
+        System.out.println("汇聚交换机流量报表格式整理完成....");
     }
 
     /**
@@ -102,11 +104,148 @@ public class ExcelCURD {
     }
 
 
+    public static void CopyOLTToReport() {
+
+        //加载OLT文件，并获取第一张工作簿
+        Workbook wbOlt = new Workbook();
+        wbOlt.loadFromFile("F:\\Datareport\\OLT流量报表.xlsx");
+        Worksheet sheetOlt = wbOlt.getWorksheets().get(0);
+
+        System.out.println("加载OLT流量报表....");
+
+        //加载流量报表，获取第6张工作表，并将OLT中的工作表内容复制到该工作表（OLT流量为第六张，索引为5）
+        Workbook wb = new Workbook();
+        wb.loadFromFile("F:\\Datareport\\接入层流量预警清单（报表整理）0330.xlsx");
+        Worksheet sheetAll = wb.getWorksheets().get(5);
+        System.out.println("将OLT流量报表迁移到总表....");
+        sheetAll.copyFrom(sheetOlt);
+
+        //保存文档
+        wb.saveToFile("F:\\Datareport\\整合OLT后流量报表.xlsx", FileFormat.Version2013);
+
+        System.out.println("OLT流量报表迁移到总表完成....");
+    }
+
+
+    //TODO：可能是表头不一样的问题，待处理
+
+    public static void CopyDSWToReport() {
+
+
+        //1、取得要复制的数据，即交换机流量报表第
+
+        //加载文档
+        Workbook wbDSW = new Workbook();
+        wbDSW.loadFromFile("F:\\Datareport\\汇聚交换机流量报表.xlsx");
+
+        Workbook wbOLT = new Workbook();
+        wbOLT.loadFromFile("F:\\Datareport\\整合OLT后流量报表.xlsx");
+
+
+        //获取第一个工作表
+        Worksheet DSWsheet = wbDSW.getWorksheets().get(0);
+
+        Worksheet OLTsheet = wbOLT.getWorksheets().get(8);
+
+
+        //获得行数,有可能包括表头需要减掉
+        Integer count = DSWsheet.getLastRow();
+
+
+        //复制指定单元格范围中的数据,此处是赋值第7列到第9列
+        CellRange sourceRange = DSWsheet.getCellRange(2, 1, count, 8);
+        CellRange destRange = OLTsheet.getCellRange(2, 1, count, 8);
+        OLTsheet.copy(sourceRange, destRange, true);
+
+        wbOLT.saveToFile("F:\\Datareport\\整合后流量报表.xlsx", FileFormat.Version2013);
+
+
+        System.out.println("完成数据迁移");
+
+    }
+
+
+    //将汇聚交换机流量报表拷贝到整合后最后一张sheet
+
+    public static void CopyDSWSheetToReport() {
+
+        //加载汇聚交换机报表，并获取第一张工作簿
+        Workbook wbOlt = new Workbook();
+        wbOlt.loadFromFile("F:\\Datareport\\汇聚交换机流量报表.xlsx");
+        Worksheet sheetOlt = wbOlt.getWorksheets().get(0);
+
+        //加载加载整合过OLT报表后的总表,并新增一张sheet用来存放汇聚交换机流量
+        Workbook wb = new Workbook();
+        wb.loadFromFile("F:\\Datareport\\整合OLT后流量报表.xlsx");
+        Worksheet cpoysheet = wb.createEmptySheet();
+
+        System.out.println("将汇聚交换机报表的内容拷贝到总表....");
+
+        //将汇聚交换机报表的内容拷贝到总表的最后一张sheet
+        cpoysheet.copyFrom(sheetOlt);
+
+        System.out.println("汇聚交换机报表的内容拷贝到总表完成....");
+
+        //保存文档
+        wb.saveToFile("F:\\Datareport\\拷贝后流量报表.xlsx", FileFormat.Version2013);
+
+
+
+
+    }
+
+
+    /**
+     * 最终整合方法，将交换机的流量拷贝到正确的位置，保持原有公式
+     */
+    public static void IntegratingReport(){
+
+
+        //将总表的最后一张sheet，也就是汇聚交换机流量数据 复制到 原 汇聚交换机流量 有公式的那一页
+
+        Workbook wbFinal = new Workbook();
+        wbFinal.loadFromFile("F:\\Datareport\\拷贝后流量报表.xlsx");
+
+        Worksheet FinalDSWsheet = wbFinal.getWorksheets().get(4);
+        Worksheet Copyedsheet = wbFinal.getWorksheets().get(9);
+
+        //获得行数
+        Integer count = Copyedsheet.getLastRow();
+
+        //表头不同，从第5行开始复制
+        CellRange sourceRange = Copyedsheet.getCellRange(5, 1, count, 8);
+        CellRange destRange = FinalDSWsheet.getCellRange(2, 1, count, 8);
+
+        System.out.println("开始最后整合....");
+
+        Copyedsheet.copy(sourceRange, destRange, true);
+
+
+        wbFinal.saveToFile("F:\\Datareport\\流量汇总表.xlsx", FileFormat.Version2013);
+
+
+        System.out.println("完成数据迁移，生成路径：F:\\Datareport\\流量汇总表.xlsx ");
+
+
+    }
+
+
+
     public static void main(String[] args) {
-       //提取汇聚交换机流量报表
-        deleteDswCol();
-       //提取OLT流量报表
+
+        //提取和整理OLT流量报表格式
         deleteOLTCol();
+        //提取整理交换机报表格式
+        deleteDswCol();
+
+        //把OLT报表导入总表
+        CopyOLTToReport();
+
+        //将交换机报表导入总表
+        CopyDSWSheetToReport();
+
+        //最终整合
+        IntegratingReport();
     }
 
 }
